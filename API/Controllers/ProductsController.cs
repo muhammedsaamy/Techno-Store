ce3393336b00c2ddbb14399217797f3cd888ccb1
muhammedsaamy <muhammedsaamy@gmail.com>
2022-12-10 21:41:47 +0200

    configDatabase

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Content("this all prod");
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetProduct(int id)
+        {
+            return Content("this one prod");
+        }
+    }
+}
