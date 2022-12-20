e0bc0b76d6adfee25611d3a633fd1d820caa5509
muhammedsaamy <muhammedsaamy@gmail.com>
2022-12-20 14:13:34 +0200

    added custom value resolver for auto mapper

---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,25 @@
+﻿using API.DTOs;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+    public class ProductUrlResolver : IValueResolver<Product, ProductToReturn, string>
+    {
+        private readonly IConfiguration _config;
+
+        public ProductUrlResolver(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        public string Resolve(Product source, ProductToReturn destination, string destMember, ResolutionContext context)
+        {
+            if (!string.IsNullOrEmpty(source.PictureUrl))
+            {
+                return _config["ApiUrl"]+source.PictureUrl;
+            }
+            return null;
+        }
+    }
+}
