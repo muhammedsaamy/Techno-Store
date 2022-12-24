9a99845840ac3d543765fd82c1685a434885ab0c
muhammedsaamy <muhammedsaamy@gmail.com>
2022-12-24 11:51:31 +0200

    Api error ahndling, creating a consistent error response

---
+++ API/Controllers/BaseApiController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Controller]
+    [Route("api/[controller]")]
+    public class BaseApiController:ControllerBase
+    {
+    }
+}
