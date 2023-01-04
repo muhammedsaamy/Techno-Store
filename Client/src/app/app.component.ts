76eb11177849c332a7d77ab96928a99a4e789723
muhammedsaamy <muhammedsaamy@gmail.com>
2023-01-04 17:07:29 +0200

    added http client module and test the data retrieved from API

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Tecno';
+  products!:any[]
+
+  constructor(private http:HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get("https://localhost:7270/api/products").subscribe({next:(response:any)=>{
+    console.log(response)
+    this.products=response.data
+    },error:(err:any)=>{
+      console.log(err)
+    }})
+  }
 }
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http'
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
@@ -14,7 +15,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
