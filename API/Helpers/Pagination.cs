b3447fcb28f7209ecfc8f3c5f70b01c753388895
muhammedsaamy <muhammedsaamy@gmail.com>
2023-01-14 15:03:02 +0200

    organising folders

--- API/Helpers/Pagination.cs
+++ API/Helpers/Pagination.cs
@@ -2,16 +2,16 @@
 {
     public class Pagination<T> where T : class
     {
-        public Pagination(int pageIndex, int pageSiza, int count, IReadOnlyList<T> data)
+        public Pagination(int pageIndex, int pageSize, int count, IReadOnlyList<T> data)
         {
             this.pageIndex = pageIndex;
-            this.pageSiza = pageSiza;
+            this.pageSize = pageSize;
             this.count = count;
             Data = data;
         }
 
         public int pageIndex { get; set; }
-        public int pageSiza { get; set; }
+        public int pageSize { get; set; }
         public int count { get; set; }
         public IReadOnlyList<T> Data { get; set; }
     }
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { Ipagination } from './models/pagination';
-import { IProduct } from './models/product';
+import { Ipagination } from './shared/models/pagination';
+import { IProduct } from './shared/models/product';
 
 @Component({
   selector: 'app-root',
