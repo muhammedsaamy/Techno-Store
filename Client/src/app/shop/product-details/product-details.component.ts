c622cdc0b47e26db7b9404ca1ac5b36367bab14f
muhammedsaamy <muhammedsaamy@gmail.com>
2023-01-20 17:40:37 +0200

    routing and product details page

--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { IProduct } from 'src/app/shared/models/product';
+import { ShopService } from '../shop.service';
 
 @Component({
   selector: 'app-product-details',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
+  product?: IProduct;
+  idParam?:number;
 
-  constructor() { }
+  constructor(private shopService:ShopService , private activatedRoute : ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.loadProduct();
   }
 
+  loadProduct()
+  {
+    this.idParam=+this.activatedRoute.snapshot.paramMap.get('id')!;
+    this.shopService.getProduct(this.idParam).subscribe(
+      {
+        next:(product)=>{this.product=product},
+        error:(err)=>{console.log(err)}
+      })
+  }
 }
