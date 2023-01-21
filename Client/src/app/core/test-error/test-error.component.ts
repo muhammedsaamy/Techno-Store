1ef7a4650e10405138bb6b784fa6cbde1bc49ace
muhammedsaamy <muhammedsaamy@gmail.com>
2023-01-21 22:07:15 +0200

    creating error components for test

---
+++ Client/src/app/core/test-error/test-error.component.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-test-error',
+  templateUrl: './test-error.component.html',
+  styleUrls: ['./test-error.component.scss']
+})
+export class TestErrorComponent implements OnInit {
+  apiUrl= environment.apiUrl;
+  constructor(private http:HttpClient) { }
+
+  ngOnInit(): void {
+  }
+
+  get404Error(){
+    this.http.get(this.apiUrl + 'products/42').subscribe({
+      next:(response)=>{
+        console.log(response)
+      },
+      error:(err)=>{
+        console.log(err)
+      }
+    })
+  }
+
+  get500Error(){
+    this.http.get(this.apiUrl + 'buggy/servererror').subscribe({
+      next:(response)=>{
+        console.log(response)
+      },
+      error:(err)=>{
+        console.log(err)
+      }
+    })
+  }
+  get400Error(){
+    this.http.get(this.apiUrl + 'buggy/badrequest').subscribe({
+      next:(response)=>{
+        console.log(response)
+      },
+      error:(err)=>{
+        console.log(err)
+      }
+    })
+  }
+  get400ValidationError(){
+    this.http.get(this.apiUrl + 'products/fortytwo').subscribe({
+      next:(response)=>{
+        console.log(response)
+      },
+      error:(err)=>{
+        console.log(err)
+      }
+    })
+  }
+}
