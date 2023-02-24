950d47105acd11e7aadb3b678b314f3368d75da2
muhammedsaamy <muhammedsaamy@gmail.com>
2023-02-24 23:36:20 +0200

    adding the product detail to the basket

--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { BasketService } from 'src/app/basket/basket.service';
+import { IBasketItem } from 'src/app/shared/models/basket';
 import { IProduct } from 'src/app/shared/models/product';
 import { ShopService } from '../shop.service';
 
@@ -9,13 +11,28 @@ import { ShopService } from '../shop.service';
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-  product?: IProduct;
+  product!: IProduct;
   idParam?:number;
+  quantity=1;
 
-  constructor(private shopService:ShopService , private activatedRoute : ActivatedRoute) { }
+  constructor(private shopService:ShopService , private activatedRoute : ActivatedRoute , private basketService:BasketService) { }
 
   ngOnInit(): void {
     this.loadProduct();
+  }
+
+  addItemToBasket(){
+    this.basketService.addItemToBasket(this.product,this.quantity)
+  }
+
+  incrementQuantity(){
+    this.quantity++;
+  }
+
+  decrementQuantity(){
+    if(this.quantity>1){
+      this.quantity--;
+    }
   }
 
   loadProduct()
