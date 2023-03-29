6cef7b98a204f37eaa3deecf1f57af6d97b9c513
muhammedsaamy <muhammedsaamy@gmail.com>
2023-03-29 20:27:23 +0200

    updating swagger config for identity

--- API/DTOs/CustomerBasketDto.cs
+++ API/DTOs/CustomerBasketDto.cs
@@ -1,6 +1,12 @@
-﻿namespace API.DTOs
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
 {
     public class CustomerBasketDto
     {
+        [Required]
+        public string Id { get; set; }
+        public List<BasketItemDto> Items { get; set; } = new List<BasketItemDto>();
+
     }
 }
--- API/Extensions/SwaggerServiceExtensions.cs
+++ API/Extensions/SwaggerServiceExtensions.cs
@@ -1,10 +1,34 @@
-﻿namespace API.Extensions
+﻿using Microsoft.OpenApi.Models;
+
+namespace API.Extensions
 {
     public static class SwaggerServiceExtensions
     {
         public static IServiceCollection AddSwaggerdocumentation (this IServiceCollection Services)
         {
-            Services.AddSwaggerGen();
+            Services.AddSwaggerGen(c =>
+            {
+                var securitySchema = new OpenApiSecurityScheme
+                {
+                    Description = "JWT Auth Bearer Scheme",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    Reference = new OpenApiReference
+                    {
+                        Type = ReferenceType.SecurityScheme,
+                        Id = "Bearer"
+                    }
+                };
+                c.AddSecurityDefinition("Bearer", securitySchema);
+                var securityRequirement = new OpenApiSecurityRequirement
+                    {
+                        { securitySchema, new[] {"Bearer" } }
+                    };
+                c.AddSecurityRequirement(securityRequirement);
+            }
+                );
             return Services;
         }
         public static IApplicationBuilder UseSwaggerDocumentation(this IApplicationBuilder app)
