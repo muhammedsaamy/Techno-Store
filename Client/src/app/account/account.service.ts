f92fee0abb1a0d61f483c2b3c81c6301d420886e
muhammedsaamy <muhammedsaamy@gmail.com>
2023-04-06 17:14:24 +0200

    persisting the login

--- Client/src/app/account/account.service.ts
+++ Client/src/app/account/account.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable, map } from 'rxjs';
@@ -13,6 +13,24 @@ export class AccountService {
   private currentUserSource = new BehaviorSubject<IUser | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
   constructor(private http:HttpClient, private router: Router) { }
+
+  getCurrentUserValue(){
+    return this.currentUserSource.value;
+  }
+
+  loadCurrentUser(token: string){
+    let headers = new HttpHeaders();
+    headers = headers.set('Authorization', `Beare ${token}`);
+
+    return this.http.get<IUser>(this.baseUrl + 'account' , {headers}).pipe(
+      map((user: IUser)=>{
+        if(user){
+          localStorage.setItem('token', user.token);
+          this.currentUserSource.next(user);
+        }
+      })
+    )
+  }
 
   login(values:any){
     return this.http.post<IUser>(this.baseUrl +'account/login' , values).pipe(
--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { BasketService } from './basket/basket.service';
+import { AccountService } from './account/account.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,9 +9,26 @@ import { BasketService } from './basket/basket.service';
 export class AppComponent implements OnInit {
   title = 'Tecno';
 
-  constructor(private basketService:BasketService) {}
+  constructor(private basketService:BasketService, private accountService: AccountService) {}
 
   ngOnInit(): void {
+    this.loadBasket();
+    this.loadCurrentUser();
+  }
+
+  loadCurrentUser(){
+    const token = localStorage.getItem('token');
+    if(token){
+      this.accountService.loadCurrentUser(token).subscribe({
+        next:()=>{
+          console.log("loaded user")},
+          error:(err)=>{console.log(err)
+        }
+      })
+    }
+  }
+
+  loadBasket(){
     const basketId= localStorage.getItem('basket_id');
     if(basketId){
       this.basketService.getBasket(basketId).subscribe({
