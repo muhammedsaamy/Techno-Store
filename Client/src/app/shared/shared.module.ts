8d4b81097b2e3ea702e81e0d6effbdc83380cd3c
muhammedsaamy <muhammedsaamy@gmail.com>
2023-04-09 01:10:11 +0200

    creating a reusable text input

--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -6,14 +6,16 @@ import { PagerComponent } from './components/pager/pager.component';
 import {CarouselModule} from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './components/order-totals/order-totals.component'
 import { ReactiveFormsModule } from '@angular/forms';
-import {BsDropdownModule} from 'ngx-bootstrap/dropdown'
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import { TextInputComponent } from './components/text-input/text-input.component'
 
 
 @NgModule({
   declarations: [
     PagingHeaderComponent,
     PagerComponent,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    TextInputComponent
   ],
   imports: [
     CommonModule,
@@ -29,7 +31,8 @@ import {BsDropdownModule} from 'ngx-bootstrap/dropdown'
     CarouselModule,
     OrderTotalsComponent,
     ReactiveFormsModule,
-    BsDropdownModule
+    BsDropdownModule,
+    TextInputComponent
   ]
 })
 export class SharedModule { }
