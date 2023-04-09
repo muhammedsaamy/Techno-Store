ce8c19c57f378fed9019c7fb0683a3cbf9f28190
muhammedsaamy <muhammedsaamy@gmail.com>
2023-04-09 15:29:36 +0200

    register form

--- Client/src/app/account/register/register.component.ts
+++ Client/src/app/account/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  registerForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private accountService: AccountService, private router: Router) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm(){
+    this.registerForm = this.fb.group({
+      displayName: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$')]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(){
+    this.accountService.register(this.registerForm.value).subscribe({
+      next:(response)=>{this.router.navigateByUrl('/shop')},
+      error: (error)=>{console.log(error)}
+    })
   }
 
 }
